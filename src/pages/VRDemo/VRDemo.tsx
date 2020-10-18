b5136faf8d68514ef6c6c8125e1f77bd63933992
Joonas Kauppinen <joonas.kauppinen@yle.fi>
2020-10-18 19:22:33 +0300

    fix(vr-demo): add better models, lights, controls and camera position in vr

--- src/pages/VRDemo/VRDemo.tsx
+++ src/pages/VRDemo/VRDemo.tsx
@@ -1,12 +1,10 @@
-import React, { useRef } from 'react';
+import React, { Suspense, useRef } from 'react';
 import styled from 'styled-components';
-import { useFrame } from 'react-three-fiber';
-import { Sky, OrbitControls } from '@react-three/drei';
+import { useFrame, useLoader, useThree } from 'react-three-fiber';
+import { Sky, OrbitControls, Html, Box, softShadows } from '@react-three/drei';
 import { VRCanvas, DefaultXRControllers } from '@react-three/xr';
 import { Mesh } from 'three';
-// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-// @ts-ignore
-// import nokia3310 from '../../assets/3D-models/nokia_3310/scene.gltf';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -14,42 +12,111 @@ const Wrapper = styled.div`
 `;
 
 const VRDemo: React.FC = () => {
+  softShadows({
+    frustrum: 3.75, // Frustrum width (default: 3.75)
+    size: 0.005, // World size (default: 0.005)
+    near: 9.5, // Near plane (default: 9.5)
+    samples: 17, // Samples (default: 17)
+    rings: 11, // Rings (default: 11)
+  });
   const Floor = (): JSX.Element => (
     <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
-      <planeBufferGeometry attach="geometry" args={[40, 40]} />
-      <meshLambertMaterial attach="material" color="#666666" />
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <shadowMaterial attach="material" opacity={1} />
     </mesh>
   );
 
-  const Box = (): JSX.Element => {
+  const UnityAdam = ({ position, rotation, scale }): JSX.Element => {
+    const { scene } = useLoader(GLTFLoader, 'gltf/adamHead/adamHead.gltf');
+    return (
+      <mesh position={position} rotation={rotation} scale={scale}>
+        <primitive object={scene} />
+      </mesh>
+    );
+  };
+
+  const Nokia3310 = ({ position }): JSX.Element => {
+    const { clock } = useThree();
     const mesh = useRef<Mesh>();
+    const { scene } = useLoader(GLTFLoader, 'gltf/nokia_3310/scene.gltf');
+    const [x, y, z] = position;
     useFrame(() => {
       if (mesh.current) {
-        mesh.current.rotation.y += 0.01;
-        mesh.current.position.y += Math.sin(Date.now() / 1000) * 0.01;
-        // if (mesh.current.position.y < 2) {
-        //   mesh.current.position.y += 0.01;
-        // }
+        mesh.current.position.y = y + Math.sin(clock.getElapsedTime()) * 0.5;
+        mesh.current.rotation.y += 0.02;
       }
     });
     return (
-      <mesh ref={mesh} scale={[0.5, 0.5, 0.5]} position={[0, 2, -2]} receiveShadow castShadow>
-        <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-        <meshLambertMaterial attach="material" color="orange" />
+      <mesh ref={mesh} position={position} scale={[0.2, 0.2, 0.2]} castShadow>
+        <primitive object={scene} />
       </mesh>
+    );
+  };
+
+  const Scene = ({ initialCamPosition }): JSX.Element => {
+    const { camera } = useThree();
+    return (
+      <>
+        <group position={initialCamPosition}>
+          <primitive object={camera} />
+        </group>
+        <Sky />
+        <ambientLight intensity={0.2} />
+        <pointLight position={[-10, 10, 10]} />
+        <pointLight position={[0, 1, -20]} intensity={0.3} />
+        <directionalLight
+          castShadow
+          position={[15, 10, 10]}
+          intensity={1}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+
+        <fog attach="fog" args={['white', 15, 25]} />
+        <Floor />
+      </>
     );
   };
 
   return (
     <Wrapper>
-      <VRCanvas shadowMap>
-        <Sky />
-        <Floor />
-        <Box />
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} castShadow />
-        <DefaultXRControllers />
-        <OrbitControls />
+      <VRCanvas colorManagement shadowMap>
+        <Suspense
+          fallback={
+            <Html>
+              <h1>loading...</h1>
+            </Html>
+          }
+        >
+          <Scene initialCamPosition={[0, 0, 2]} />
+          <OrbitControls maxPolarAngle={Math.PI / 2.2} />
+          <Box position={[-2, 1, 0]} castShadow>
+            <meshStandardMaterial attach="material" color="orange" />
+            <Html center position={[0, 2, 0]} scaleFactor={8}>
+              <div
+                style={{
+                  padding: '0px 13px',
+                  backgroundColor: 'white',
+                  borderColor: '#dddddd',
+                  borderStyle: 'solid',
+                  borderWidth: 1,
+                  borderRadius: 9,
+                }}
+              >
+                <h1>Hello!</h1>
+                <p>I'm a cube and I know how to cast a shadow.</p>
+              </div>
+            </Html>
+          </Box>
+          <Nokia3310 position={[0, 1.5, 0]} />
+          <UnityAdam position={[3, 2.5, 0]} rotation={[0, Math.PI / 1.5, 0]} scale={[1, 1, 1]} />
+          <DefaultXRControllers />
+        </Suspense>
       </VRCanvas>
     </Wrapper>
   );
