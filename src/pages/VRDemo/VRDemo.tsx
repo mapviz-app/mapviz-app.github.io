3d644548ccf35baf33cd5843408bac7d55cc013f
Joonas Kauppinen <joonas.kauppinen@yle.fi>
2020-10-25 21:27:48 +0200

    feat(vr-demo): add cabin model and enable shadows on gltf models

--- src/pages/VRDemo/VRDemo.tsx
+++ src/pages/VRDemo/VRDemo.tsx
@@ -1,10 +1,12 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { Suspense, useRef } from 'react';
 import styled from 'styled-components';
-import { useFrame, useLoader, useThree } from 'react-three-fiber';
+import { useFrame, useThree } from 'react-three-fiber';
 import { Sky, OrbitControls, Html, Box, softShadows } from '@react-three/drei';
 import { VRCanvas, DefaultXRControllers } from '@react-three/xr';
 import { Mesh } from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import Nokia3310 from '../../assets/gltfjsx/Nokia3310';
+import Cabin from '../../assets/gltfjsx/CabinInTheWoods';
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -14,11 +16,12 @@ const Wrapper = styled.div`
 const VRDemo: React.FC = () => {
   softShadows({
     frustrum: 3.75, // Frustrum width (default: 3.75)
-    size: 0.005, // World size (default: 0.005)
-    near: 9.5, // Near plane (default: 9.5)
+    size: 0.007, // World size (default: 0.005)
+    near: 9.4, // Near plane (default: 9.5)
     samples: 17, // Samples (default: 17)
-    rings: 11, // Rings (default: 11)
+    rings: 10, // Rings (default: 11)
   });
+
   const Floor = (): JSX.Element => (
     <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
       <planeBufferGeometry attach="geometry" args={[100, 100]} />
@@ -26,28 +29,18 @@ const VRDemo: React.FC = () => {
     </mesh>
   );
 
-  const UnityAdam = ({ position, rotation, scale }): JSX.Element => {
-    const { scene } = useLoader(GLTFLoader, 'gltf/adamHead/adamHead.gltf');
-    return (
-      <mesh position={position} rotation={rotation} scale={scale}>
-        <primitive object={scene} />
-      </mesh>
-    );
-  };
-
-  const Nokia3310 = ({ position }): JSX.Element => {
+  const BouncyNokia = (props): JSX.Element => {
     const { clock } = useThree();
     const mesh = useRef<Mesh>();
-    const { scene } = useLoader(GLTFLoader, 'gltf/nokia_3310/scene.gltf');
     useFrame(() => {
       if (mesh.current) {
-        mesh.current.position.y = position[1] + Math.sin(clock.getElapsedTime()) * 0.5;
+        mesh.current.position.y = props.position[1] + Math.sin(clock.getElapsedTime()) * 0.5;
         mesh.current.rotation.y += 0.02;
       }
     });
     return (
-      <mesh ref={mesh} position={position} scale={[0.2, 0.2, 0.2]} castShadow>
-        <primitive object={scene} />
+      <mesh ref={mesh}>
+        <Nokia3310 {...props} />;
       </mesh>
     );
   };
@@ -61,19 +54,19 @@ const VRDemo: React.FC = () => {
         </group>
         <Sky />
         <ambientLight intensity={0.2} />
-        <pointLight position={[-10, 10, 10]} />
-        <pointLight position={[0, 1, -20]} intensity={0.3} />
+        {/* <pointLight position={[-10, 10, 10]} /> */}
+        <pointLight position={[0, 1, -20]} intensity={0.1} />
         <directionalLight
           castShadow
           position={[15, 10, 10]}
-          intensity={1}
-          shadow-mapSize-width={1024}
-          shadow-mapSize-height={1024}
-          shadow-camera-far={50}
-          shadow-camera-left={-10}
-          shadow-camera-right={10}
-          shadow-camera-top={10}
-          shadow-camera-bottom={-10}
+          intensity={0.7}
+          // shadow-mapSize-width={1024}
+          // shadow-mapSize-height={1024}
+          // shadow-camera-far={50}
+          // shadow-camera-left={-10}
+          // shadow-camera-right={10}
+          // shadow-camera-top={10}
+          // shadow-camera-bottom={-10}
         />
 
         <fog attach="fog" args={['white', 15, 25]} />
@@ -94,9 +87,9 @@ const VRDemo: React.FC = () => {
         >
           <Scene initialCamPosition={[0, 0, 2]} />
           <OrbitControls maxPolarAngle={Math.PI / 2.2} />
-          <Box position={[-2, 1, 0]} castShadow>
+          <Box position={[-2, 0.5, 0]} castShadow>
             <meshStandardMaterial attach="material" color="orange" />
-            <Html center position={[0, 2, 0]} scaleFactor={8}>
+            {/* <Html center position={[0, 2, 0]} scaleFactor={8}>
               <div
                 style={{
                   padding: '0px 13px',
@@ -108,12 +101,12 @@ const VRDemo: React.FC = () => {
                 }}
               >
                 <h1>Hello!</h1>
-                <p>I'm a cube and I know how to cast a shadow.</p>
+                <p>I&apos;m a cube and I know how to cast a shadow.</p>
               </div>
-            </Html>
+            </Html> */}
           </Box>
-          <Nokia3310 position={[0, 1.5, 0]} />
-          <UnityAdam position={[3, 2.5, 0]} rotation={[0, Math.PI / 1.5, 0]} scale={[1, 1, 1]} />
+          <BouncyNokia position={[0, 1, 0]} scale={[0.15, 0.15, 0.15]} />
+          <Cabin position={[5, 0.25, 2]} />
           <DefaultXRControllers />
         </Suspense>
       </VRCanvas>
